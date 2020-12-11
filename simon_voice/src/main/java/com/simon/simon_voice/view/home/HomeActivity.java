65e1720d93252c3a20d2a5300e171baedfff3376
Simon <349283970@qq.com>
2020-12-11 12:33:58 +0800

    初始化主页面

---
+++ simon_voice/src/main/java/com/simon/simon_voice/view/home/HomeActivity.java
@@ -0,0 +1,28 @@
+package com.simon.simon_voice.view.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.simon.simon_voice.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        initView();
+        initData();
+    }
+
+
+    private void initView() {
+
+    }
+
+
+    private void initData() {
+
+    }
+}
